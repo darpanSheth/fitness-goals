e6ab246b48c9de12af7291dd6a4291cc7a5d788d
darpanSheth <darpansheth88@gmail.com>
2022-09-25 20:58:38 -0500

    Updated Javascript file
    Added onclick attributes to toggle visibility of services offered according to selection from user.

--- index.js
+++ index.js
@@ -69,10 +69,93 @@ function calculateBMI(form){
 
 /* The segment below is for displaying the visibility of the options selected on the services page*/
 
-const store = new Map([
-    ["tshirt", 20],
-    ["chaddi", 30],
-    ["banyan", 40]
-])
+const allInOne = document.getElementById("offer-checkbox");                                 //selected the allInOne offer checkbox
+const container5 = document.getElementsByClassName("bill-service");                         //selected whole bottom container.
+const amountLine = document.getElementById("amount-footer");                                //selected the amount line
+const listbox = document.getElementById("services-options");                                //selected all the options from the listbox
+const proceedButton = document.getElementById("proceed-button");                            //selected the proceed button
+const personalTrainingSection = document.getElementById("personalTraining");
+const dieticianSection = document.getElementById("dietician");
+const supplimentsPackageSection = document.getElementById("supplimentsPackage");
+const yogaSessionsSection = document.getElementById("yogaSessions");
+const zumbaAerobicsSection = document.getElementById("zumbaAerobics");
 
-store.forEach((value, key) => console.log(store.get(key)))
+allInOne.addEventListener("click", () => {
+    if(allInOne.checked){
+        for(let containers of container5){
+            console.log(containers);
+            containers.classList.add("bill-service-flex");
+        }
+        amountLine.style.display = "block";
+        listbox.disabled = true;
+        proceedButton.disabled = true;
+
+        document.getElementById("total-amount").innerHTML = "<del>$305</del> $280";
+    }
+    else{
+        for(let containers of container5){
+            containers.classList.remove("bill-service-flex");
+        }
+        amountLine.style.display = "none";
+        listbox.disabled = false;
+        proceedButton.disabled = false;
+        
+    }
+    
+})
+
+proceedButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let totalCost = 0;
+    const servicePrices = {
+        "Personal training": 100,
+        "Dietician": 60,
+        "Suppliments package": 75,
+        "Yoga sessions": 30,
+        "Zumba Aerobics": 40
+    }
+
+    for(let containers of container5){
+        containers.classList.remove("bill-service-flex");
+    }
+    amountLine.style.display = "none";
+
+    for(let selectedOption of listbox.selectedOptions){
+        const key = selectedOption.value;
+        const price = servicePrices[key];
+        totalCost += price;
+        document.getElementById("total-amount").innerText = "$" + totalCost;
+        amountLine.style.display = "block";
+        switch (key) {
+            case "Personal training":
+                personalTrainingSection.classList.add("bill-service-flex");
+                break;
+             
+            case "Dietician":
+                dieticianSection.classList.add("bill-service-flex");
+                break;
+            
+            case "Suppliments package":
+                supplimentsPackageSection.classList.add("bill-service-flex");
+                break;
+
+            case "Yoga sessions":
+                yogaSessionsSection.classList.add("bill-service-flex");
+                break;
+
+            case "Zumba Aerobics":
+                zumbaAerobicsSection.classList.add("bill-service-flex");
+                break;
+        
+            default:
+                break;
+        }      
+    };
+    
+
+    e.target.form.reset();
+
+})
+
+
+
